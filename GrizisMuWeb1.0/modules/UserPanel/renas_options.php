9c151cfe426f2a3ad8b063afad460388b9093d0b
hrisiz <hrisi.zarkov@abv.bg>
2014-02-11 19:46:28 +0200

    Web 0.3

--- GrizisMuWeb1.0/modules/UserPanel/renas_options.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$path_parts = pathinfo(__FILE__);
-preg_match("/".$path_parts['basename']."/", $_SERVER['SCRIPT_NAME'], $matches);
-if (!empty($matches[0])){header("Location: /?page=News");}
-echo"<br><br><br>";
-read_files_from_folder("modules/UserPanel/renas","renas_options");
-include_once open_modul("modules/UserPanel/renas/","","renas_options");
-?>
--- GrizisMuWeb1.0/right_menu/News/all_new.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-$path_parts = pathinfo(__FILE__);
-preg_match("/".$path_parts['basename']."/", $_SERVER['SCRIPT_NAME'], $matches);
-if (!empty($matches[0])){header("Location: /?page=News");}
-print "xaxa";
-?>
