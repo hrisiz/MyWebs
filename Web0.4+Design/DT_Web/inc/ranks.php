0d544635e471b296362bc46bbe0a390728eb9bcd
hrisiz <hrisi.zarkov@abv.bg>
2014-05-03 02:00:28 +0300

    Delete

--- Web0.4+Design/DT_Web/inc/ranks.php
+++
@@ -1,6 +0,0 @@
-<ul class="side_menu">
-	<li><a href="?p=topchars">Top Characters</a></li>
-	<li><a href="?p=topguilds">Top Guilds</a></li>
-	<li><a href="?p=topkillers">Top Killers</a></li>
-	<li><a href="?p=gamemasters">Game Masters</a></li>
-</ul>
